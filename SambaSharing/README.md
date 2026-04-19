0d27b842447526859a59f01407cfb7dc8c5913cc
mcfly1885 <275965599+mcfly1885@users.noreply.github.com>
2026-04-19 13:50:10 +0200

    Add readme and first script

---
+++ SambaSharing/README.md
@@ -0,0 +1,21 @@
+# Samba Share Configuration Wizard 🪄
+The script that creates a new samba share.
+
+## How it works ⚙️
+
+The scripts requires to user the following parameters:
+
+- name of Samba share
+- path to directory to share
+- permissions (from 000 to 777)
+- a comment
+
+Once the paramter has been given, the script shows the resume of user entry. If user confirm, the script will write the parameters on **/etc/samba/smb.conf** file, otherwise will abort the operation.
+
+**Make sure to restart smbd service for the changes to take effect.**
+
+## Why this script? 💡
+Because it can makes this operation faster then hand writing each parameter in smb.conf file.
+
+## Be carful! ⚠️
+**Be carful of what are you doing!** This script will edit a conf file and you need to be sudo.
---
+++ SambaSharing/sambasharing.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+#Script that creates a samba sharing with given parameters
+
+echo -e "\n"
+
+#check if user is sudo or root
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then 
+	echo "ha ha ha, you didn't say the magic word..."
+	echo "Access Denied"
+	exit;
+fi
+
+SMB_CONFIG_FILE="/etc/samba/smb.conf"
+
+
+echo -e "Samba Share Configuration Wizard\n********************************"
+
+#get the parameters
+
+read -p "Enter Sharing Name: " sharingName
+read -p "Enter Sharing Path: " sharingPath
+read -p "Set permission (000 - 777):" sharingMask
+read -p "Comment (leave blank if not): " sharingComment
+
+sharingGuest=no
+sharingBrowsable=yes
+
+
+#resume parameters
+printf "\n----------------------------------\n"
+echo -e "The following Samba sharing will be add:"
+echo -e "\n"
+echo -e "[$sharingName]"
+echo -e "path = $sharingPath"
+echo -e "browsable = $sharingBrowsable"
+echo -e "directory mask = $sharingMask"
+echo -e "guest ok	= $sharingGuest"
+echo -e "comment = $sharingComment" 
+
+#ask for confirmation
+
+while true; do
+	printf "\n\n"
+	read -p "Continue? y/n " yn
+	case $yn in
+		[Yy]* )
+	printf "\n" >> $SMB_CONFIG_FILE
+	echo "[$sharingName]" >> $SMB_CONFIG_FILE
+	echo "path = $sharingPath" >> $SMB_CONFIG_FILE
+	echo "comment = $sharingComment" >> $SMB_CONFIG_FILE
+	echo "browsable = yes" >> $SMB_CONFIG_FILE
+	echo "directory mask = $sharingMask" >> $SMB_CONFIG_FILE
+	echo "guest ok = $sharingGuest" >> $SMB_CONFIG_FILE
+	printf "\n\n"
+	echo "Done"
+	break;;
+		[Nn]* )
+			printf "\nAborted\n"
+			exit
+			break;;
+		* )
+			echo "please enter y or n"
+	esac
+done
